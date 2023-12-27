2e68c22fc109448194435f13c983c0a2ef3ff197
Kevin Garanger <kevin@garanger.fr>
2023-12-27 13:58:34 -0800

    update license

--- scripts/train.py
+++ scripts/train.py
@@ -1,3 +1,17 @@
+# Copyright 2023 Kévin Garanger
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import argparse
 import logging
 import re
@@ -409,8 +423,6 @@ def create_train_state(
     )
 
     logging.info(f"Model parameters dtype: {params_types}")
-    # f"{next(flax.traverse_util.TraverseTree().iterate(train_state_params)).dtype}"
-    # )
 
     logging.info(
         "Model parameters count: "
